dc174b7615a97f0bd64468c6cec2cf9117a8c975
James <j.g.mcadam@gmail.com>
2017-03-01 15:24:45 +0000

    Music Manager for the Game and Main Menu
    Main Menu loads into the game. Music Manager runs in the background -
    NOTE THE AUDIO WILL RESULT IN NULL REFERENCE AS IT IS TOO BIG FOR
    GITHUB.

---
+++ TimeDefense/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+	public AudioClip music;
+
+	// Use this for initialization
+	void Start () {
+
+		DontDestroyOnLoad(this);
+
+		gameObject.GetComponent<AudioSource> ().PlayOneShot(music);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (gameObject.GetComponent<AudioSource> ().isPlaying == false)
+		{
+			gameObject.GetComponent<AudioSource> ().PlayOneShot(music);
+		}
+	}
+}
