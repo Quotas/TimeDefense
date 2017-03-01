c7853906220c82c6807a41a66b0ca4f78689f35c
Seamus <seamussalt@gmail.com>
2017-03-01 16:33:17 +0000

    Wave bar fixed

--- TimeDefense/Assets/Scripts/MainMenu.cs
+++ TimeDefense/Assets/Scripts/MainMenu.cs
@@ -8,7 +8,7 @@ public class MainMenu : MonoBehaviour {
 	public void StartGame()
 	{
 
-		SceneManager.LoadScene ("level", LoadSceneMode.Single);
+		SceneManager.LoadScene ("Level", LoadSceneMode.Single);
 
 	}
 
---
+++ TimeDefense/Assets/Scripts/WaveBar.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveBar : MonoBehaviour {
+
+    public Sprite wave1;
+    public Sprite wave2;
+    public Sprite wave3;
+
+    public Image waveImage;
+    public Level level;
+
+    void Start()
+    {
+        waveImage = transform.FindChild("WaveImage").GetComponent<Image>();
+        level = FindObjectOfType<Level>();
+
+    }
+
+
+    // Update is called once per frame
+    void Update () {
+
+        switch (level.curWave) {
+            case 1:
+                waveImage.sprite = wave1;
+                break;
+            case 2:
+                waveImage.sprite = wave2;
+                break;
+            case 3:
+                waveImage.sprite = wave3;
+                break;
+
+
+
+
+        }
+
+	}
+}
